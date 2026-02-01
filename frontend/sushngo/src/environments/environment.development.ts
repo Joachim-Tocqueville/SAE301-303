b62475ca03ca65a4564e9fc9b39c56d9533394a6
Joachim Tocqueville <joachim.tocqueville@gmail.com>
2026-02-01 19:29:19 +0100

    Ajout du lien de l'API hébergé

---
+++ frontend/sushngo/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://sae301-303.onrender.com/api.php'
+};
---
+++ frontend/sushngo/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost/SAE301-303/backend/api/api.php'
+};
